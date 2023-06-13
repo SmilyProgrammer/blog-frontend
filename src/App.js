977c3d27ed19926b1de915cef62381aadf1e6cb8
Hassan Murtaza <133641992+SmilyProgrammer@users.noreply.github.com>
2023-06-13 01:41:48 -0700

    CustamNavbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomNavbar from "./components/CustomNavbar";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomNavbar />
+    </div>
+  );
+}
+
+export default App;
