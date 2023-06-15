78bef32f61d57952c61fcc0597641a553f66bc3d
Hassan Murtaza <133641992+SmilyProgrammer@users.noreply.github.com>
2023-06-14 23:55:12 -0700

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CustomNavbar from "./components/CustomNavbar";
 
 function App() {
+  // console.log(axios.isCancel("something"));
   return (
     <div className="App">
       <CustomNavbar />
